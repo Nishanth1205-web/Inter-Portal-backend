b0f2cbeaacbebed78711fc93de17e2d72d44eae8
Nishanth1205-web <nishantharasu1205@gmail.com>
2026-09-19 17:25:59 +0530

    Fix CORS origin issue by allowing true

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const app = express();
 // Security
 app.use(helmet());
 app.use(cors({
-  origin: env.CORS_ORIGIN,
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
